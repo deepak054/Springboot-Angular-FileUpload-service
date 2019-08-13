b23201ec2ff45a737870f810c120df06465ba183
deepak <deepak@github.com>
2019-08-13 11:53:29 +0530

    Initial commit

---
+++ src/main/java/com/deepak/angularfileuploadservice/AngularFileuploadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.angularfileuploadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularFileuploadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularFileuploadServiceApplication.class, args);
+	}
+
+}
